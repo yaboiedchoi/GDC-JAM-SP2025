17900bec4791b75707317eb2139b0a50c2f90e74
Eggy <davidfleniken443@gmail.com>
2025-03-31 09:13:00 -0400

    Adjusted some stuff

--- GDC-JAM-SP2025/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ GDC-JAM-SP2025/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -223,13 +223,15 @@ public class PlayerMovement : MonoBehaviour
             velo.y += yIncrementalVelo * yVeloDirection * hoverMod * Time.deltaTime;
         }
         rb.linearVelocity = velo;
-        debugInfo();
+        //debugInfo();
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+   
+    private void OnTriggerEnter2D(Collider2D col)
     {
         if( col.gameObject.tag == platformTag && curPhase == JumpPhase.Acceleration)
         {
+            
             curPhase = JumpPhase.Fall;
         }
     }
